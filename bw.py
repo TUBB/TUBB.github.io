0453dbe2c6347ae520de0340d50b8caebf6e96e2
TUBB <tubingbing@wallan-tech.com>
2018-04-10 23:31:12 +0800

    Site updated: 2018-04-10 23:31:12

---
+++ bw.py
@@ -0,0 +1,6 @@
+import base64
+
+about_me = '''Cj09PT09PT09Peiwouiwouafpeeciz09PT09PT09CgrmioDmnK/ljZrlrqLvvJpodHRwOi8vdHVi
+           Yi5naXRodWIuaW8KR2l0SHVi77yaaHR0cDovL2dpdGh1Yi5jb20vdHViYgpFbWFpbO+8mnR1YmdiZ0B
+           nbWFpbC5jb20K5b6u5L+h5Y+377yacG9iaXRvbwo='''
+print(base64.decodestring(about_me))
